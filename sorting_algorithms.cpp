0bad8a60052df18556b0107616af5c630370266d
Adi987p <119767109+Adi987p@users.noreply.github.com>
2024-07-26 15:59:59 +0530

    Update sorting_algorithms.cpp

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -62,3 +62,55 @@ void insertionsort(vector<int>&v)
     }
 }
 
+//4.MergeSort
+
+void merge(vector<int>&v,int start,int mid,int end)
+{
+  vector<int>temp;
+  int l=start;
+   int r=mid+1;
+  
+  while(l<=mid && r<=end)
+  {
+      if(v[l]<v[r])
+      {
+         temp.push_back(v[l]);
+         l++;
+      }
+      else
+      {
+          temp.push_back(v[r]);
+          r++;
+      }
+      
+      while(l<=mid)
+       { temp.push_back(v[l]);
+           l++;
+       }
+       while(r<=end)
+      {
+          temp.push_back(v[r]);
+          r++;
+      }
+  }
+  for(int i=start;i<=end;i++)
+    {
+        v[i]=temp[i-start];
+    }
+}
+
+void partition(vector<int>&v,int l,int h)
+{ 
+    if(l>=h)
+        return;
+    int mid = l+(h-l)/2;
+    partition(v,l,mid);
+    partition(v,mid+1,h);
+    merge(v,l,mid,h);
+}
+void mergesort(vector<int>&v)
+{
+    int n=v.size();
+    partition(v,0,n-1);
+}
+
