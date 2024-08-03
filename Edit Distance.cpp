0f12d254db1506eb27bbb11e9beec96c8cd48834
Adi987p <119767109+Adi987p@users.noreply.github.com>
2024-08-03 18:06:47 +0530

    Update Edit Distance.cpp

--- Edit Distance.cpp
+++ Edit Distance.cpp
@@ -88,3 +88,56 @@ class Solution {
         }
         return dp[0][0];
     }
+
+//Space Optimization
+
+class Solution {
+  public:
+    int solTab(string str1,string str2)
+    {
+        vector<int>curr(str2.size()+1,0);
+        vector<int>next(str2.size()+1,0);
+        
+        for(int j=0;j<str2.size();j++)
+        {
+            next[j]=str2.size()-j;
+        }
+        
+        for(int i=str1.size()-1;i>=0;i--)
+        {
+            for(int j=str2.size()-1;j>=0;j--)
+            {
+                curr[str2.size()]=str1.size()-i;
+                int ans=0;
+                if(str1[i]==str2[j])
+                    ans=next[j+1];
+        
+                 else
+                {
+                    //insert
+                    int insertAns= 1+curr[j+1];
+            
+                    //delete
+                    int deleteAns= 1+next[j];
+            
+                    //replace
+                    int replaceAns= 1+next[j+1];
+            
+                    ans=min(insertAns,min(deleteAns,replaceAns));
+                }
+                curr[j]=ans;
+                
+            }
+            next=curr;
+        }
+        return next[0];
+    }
+    int editDistance(string str1, string str2) {
+        // Code here
+        if(str1.size()==0)
+            return str2.size();
+        if(str2.size()==0)
+            return str1.size();
+        return solTab(str1,str2);
+    }
+};
