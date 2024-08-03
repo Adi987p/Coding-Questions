42dccf8b644b7f9dc88e67d6588b74cae5884edb
Adi987p <119767109+Adi987p@users.noreply.github.com>
2024-08-03 17:49:38 +0530

    Update Edit Distance.cpp

--- Edit Distance.cpp
+++ Edit Distance.cpp
@@ -1,5 +1,5 @@
 // https://www.geeksforgeeks.org/problems/edit-distance3702/1
-
+//Memoization
 class Solution {
   public:
     int solve(string str1,string str2,int i,int j,vector<vector<int>>&dp)
@@ -45,3 +45,46 @@ class Solution {
         return solve(str1,str2,0,0,dp);
     }
 };
+
+//Tabulation;
+    int solTab(string str1,string str2)
+    {
+        vector<vector<int>>dp(str1.size()+1,vector<int>(str2.size()+1,0));
+        
+        for(int j=0;j<str2.size();j++)
+        {
+            dp[str1.size()][j]=str2.size()-j;
+        }
+        
+        for(int i=0;i<str1.size();i++)
+        {
+            dp[i][str2.size()]=str1.size()-i;
+        }
+        
+        for(int i=str1.size()-1;i>=0;i--)
+        {
+            for(int j=str2.size()-1;j>=0;j--)
+            {
+                int ans=0;
+                if(str1[i]==str2[j])
+                    ans=dp[i+1][j+1];
+        
+                 else
+                {
+                    //insert
+                    int insertAns= 1+dp[i][j+1];
+            
+                    //delete
+                    int deleteAns= 1+dp[i+1][j];
+            
+                    //replace
+                    int replaceAns= 1+dp[i+1][j+1];
+            
+                    ans=min(insertAns,min(deleteAns,replaceAns));
+                }
+                dp[i][j]=ans;
+                
+            }
+        }
+        return dp[0][0];
+    }
